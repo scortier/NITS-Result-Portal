64659cca5bfdfdf845a3c84d4242187661e71b3c
sameer_99 <sameershrivastava46@gmail.com>
2020-11-06 00:36:55 +0530

    added prettier file

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,25 +1,23 @@
-const User=require("../models/User");
-const jwt=require("jsonwebtoken");
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 
-module.exports= async(req,res,next)=>{
-
-    try{
+module.exports = async (req, res, next) => {
+    try {
         const token = req.cookies.authorization
-        const userInfo = await jwt.verify(token,process.env.JWT_SECRET);
+        const userInfo = await jwt.verify(token, process.env.JWT_SECRET)
         const user = await User.findById(userInfo._id)
-        if(!user){
-            throw new Error("User not found") 
+        if (!user) {
+            throw new Error('User not found')
         }
-        req.userInfo={
-            token:token,
-            user:user
+        req.userInfo = {
+            token: token,
+            user: user,
         }
-        next();
-    }
-    catch(err){
+        next()
+    } catch (err) {
         res.status(401).json({
-            error:true,
-            message:"Login First"
-        });
+            error: true,
+            message: 'Login First',
+        })
     }
 }
