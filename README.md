cf23a0d7afe67e237e751cb71d2f6d740e9170d3
sameer_99 <sameershrivastava46@gmail.com>
2020-11-06 00:24:31 +0530

    cleared master

--- README.md
+++
@@ -1,7 +0,0 @@
-# NITS-Result-Portal [Ongoing]
-
-## MASTER BRANCH
-`Don't Mess With Master`
-
-## Dev Branch
-`Do required changes only in Dev`
--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const authController = require("../controllers/authController");
-const router = express.Router();
-
-router.route("/signup").post(authController.signup);
-router.route("/signin").post(authController.signin);
-router.route("/signout").post(authController.signout);
-
-module.exports = router;
