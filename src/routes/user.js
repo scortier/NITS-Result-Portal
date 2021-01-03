ebece67af96513982b6621427223e5add6b05506
Gaurav Das <gauravdas014@gmail.com>
2021-01-03 11:28:41 +0530

    add fileUpload middleware function to the upload img route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 /** Write User routes here */
 const router = require('express').Router()
 const userController = require('../controllers/user')
+const upload = require('../utils/uploadToS3')
 const { auth, isLoggedIn } = require('../middlewares/auth')
 
 router.post('/login', userController.login_post)
@@ -12,6 +13,7 @@ router.get('/settings', auth, userController.settings_get)
 router.post(
     '/settings/profile/image/:userId',
     auth,
+    upload.single('profileImage'),
     userController.changeProfileImage
 )
 
